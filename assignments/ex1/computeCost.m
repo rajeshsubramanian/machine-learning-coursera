f2e1b3531e76c99e1aff70247d45fa70fe7af985
Rajesh Subramanian <rsubramanian@resource.com>
2016-07-23 20:25:06 -0400

    Removed no-wormy code

--- assignments/ex1/computeCost.m
+++ assignments/ex1/computeCost.m
@@ -8,14 +8,6 @@ m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
 J = (sum((X*theta - y).^2))/(2*m);
-%J = 0;
-%total = 0;
-%value = 0;
-%for (i = 1: size(y))
-%  value = (theta(1) + theta(2)*X(i) - y(i))^2;
-%  total += value^2;
-%end
-%J = total/(2*m);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
